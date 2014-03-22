f91f4b4c8305e43bb5b1b1257a973713d91516d8
Ola Petersson <olbpetersson@gmail.com>
2014-03-22 18:18:07 +0100

    modifications to bootstrap and responsiveness - maxheigt etc

--- js/app.js
+++ js/app.js
@@ -7,23 +7,23 @@ angular.module("app", [
   $stateProvider
     .state("home",{
       url: "/",
-      templateUrl: "templates/home.html"
+      templateUrl: "/home.html"
     })
     .state("career",{
       url: "/career",
-      templateUrl: "templates/career.html"
+      templateUrl: "/templates/career.html"
     })
     .state("projects",{
-      url: "/prjects",
-      templateUrl: "templates/projects.html"
+      url: "/projects",
+      templateUrl: "/templates/projects.html"
     })
     .state("blog",{
       url: "/blog",
-      templateUrl: "templates/blog.html"
+      templateUrl: "/templates/blog.html"
     })
     .state("contact",{
       url: "/contact",
-      templateUrl: "templates/contact.html"
+      templateUrl: "/templates/contact.html"
     })
 
 }])
